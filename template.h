c741a014039eeeea00a198859b9100b0edafefad
惊蛰 <100140586+o-i-i-o@users.noreply.github.com>
2025-07-21 12:14:49 +0800

    Add files via upload

---
+++ template.h
@@ -0,0 +1,320 @@
+/* Automagically generated, do not edit! */
+buffer: v
+buffer: t
+buffer: t
+buffer: 0
+buffer: 0
+buffer: 1
+buffer: 1
+buffer: _
+buffer: _
+buffer: h
+buffer: h
+buffer: e
+buffer: e
+buffer: a
+buffer: a
+buffer: d
+buffer: d
+buffer: 1
+buffer: 1
+buffer: -
+static const u_char t01_head1[] = ""
+"<!DOCTYPE html>"
+"<html>"
+"<head>"
+"<meta http-equiv=\"content-type\" content=\"text/html; charset=utf-8\">"
+"<meta name=\"viewport\" content=\"width=device-width\">"
+"<style type=\"text/css\">"
+"body,html {"
+"background:#fff;"
+"font-family:\"Bitstream Vera Sans\",\"Lucida Grande\","
+"\"Lucida Sans Unicode\",Lucidux,Verdana,Lucida,sans-serif;"
+"}"
+"tr:nth-child(even) {"
+"background:#f4f4f4;"
+"}"
+"th,td {"
+"padding:0.1em 0.5em;"
+"}"
+"th {"
+"text-align:left;"
+"font-weight:bold;"
+"background:#eee;"
+"border-bottom:1px solid #aaa;"
+"}"
+"#list {"
+"border:1px solid #aaa;"
+"width:100%;"
+"}"
+"a {"
+"color:#a33;"
+"}"
+"a:hover {"
+"color:#e33;"
+"}"
+"</style>"
+"\n"
+buffer: v
+buffer: t
+buffer: t
+buffer: 0
+buffer: 0
+buffer: 2
+buffer: 2
+buffer: _
+buffer: _
+buffer: h
+buffer: h
+buffer: e
+buffer: e
+buffer: a
+buffer: a
+buffer: d
+buffer: d
+buffer: 2
+buffer: 2
+buffer: -
+;
+static const u_char t02_head2[] = ""
+"\n"
+"<title>Index of "
+buffer: v
+buffer: N
+buffer: N
+buffer: O
+buffer: O
+buffer: N
+buffer: N
+buffer: E
+buffer: E
+buffer: -
+;
+buffer: v
+buffer: t
+buffer: t
+buffer: 0
+buffer: 0
+buffer: 3
+buffer: 3
+buffer: _
+buffer: _
+buffer: h
+buffer: h
+buffer: e
+buffer: e
+buffer: a
+buffer: a
+buffer: d
+buffer: d
+buffer: 3
+buffer: 3
+buffer: -
+static const u_char t03_head3[] = ""
+"</title>"
+"\n"
+"</head>"
+buffer: v
+buffer: t
+buffer: t
+buffer: 0
+buffer: 0
+buffer: 4
+buffer: 4
+buffer: _
+buffer: _
+buffer: b
+buffer: b
+buffer: o
+buffer: o
+buffer: d
+buffer: d
+buffer: y
+buffer: y
+buffer: 1
+buffer: 1
+buffer: -
+;
+static const u_char t04_body1[] = ""
+"<body>"
+"<h1>Index of "
+buffer: v
+buffer: N
+buffer: N
+buffer: O
+buffer: O
+buffer: N
+buffer: N
+buffer: E
+buffer: E
+buffer: -
+;
+buffer: v
+buffer: t
+buffer: t
+buffer: 0
+buffer: 0
+buffer: 5
+buffer: 5
+buffer: _
+buffer: _
+buffer: b
+buffer: b
+buffer: o
+buffer: o
+buffer: d
+buffer: d
+buffer: y
+buffer: y
+buffer: 2
+buffer: 2
+buffer: -
+static const u_char t05_body2[] = ""
+"</h1>"
+"\n"
+buffer: v
+buffer: t
+buffer: t
+buffer: 0
+buffer: 0
+buffer: 6
+buffer: 6
+buffer: _
+buffer: _
+buffer: l
+buffer: l
+buffer: i
+buffer: i
+buffer: s
+buffer: s
+buffer: t
+buffer: t
+buffer: 1
+buffer: 1
+buffer: -
+;
+static const u_char t06_list1[] = ""
+"<table id=\"list\">"
+"<thead>"
+"<tr>"
+"<th style=\"width:55%\"><a href=\"?C=N&amp;O=A\">File Name</a>&nbsp;<a href=\"?C=N&amp;O=D\">&nbsp;&darr;&nbsp;</a></th>"
+"<th style=\"width:20%\"><a href=\"?C=S&amp;O=A\">File Size</a>&nbsp;<a href=\"?C=S&amp;O=D\">&nbsp;&darr;&nbsp;</a></th>"
+"<th style=\"width:25%\"><a href=\"?C=M&amp;O=A\">Date</a>&nbsp;<a href=\"?C=M&amp;O=D\">&nbsp;&darr;&nbsp;</a></th>"
+"</tr>"
+"</thead>"
+"\n"
+"<tbody>"
+buffer: v
+buffer: t
+buffer: t
+buffer: _
+buffer: _
+buffer: p
+buffer: p
+buffer: a
+buffer: a
+buffer: r
+buffer: r
+buffer: e
+buffer: e
+buffer: n
+buffer: n
+buffer: t
+buffer: t
+buffer: d
+buffer: d
+buffer: i
+buffer: i
+buffer: r
+buffer: r
+buffer: _
+buffer: _
+buffer: e
+buffer: e
+buffer: n
+buffer: n
+buffer: t
+buffer: t
+buffer: r
+buffer: r
+buffer: y
+buffer: y
+buffer: -
+;
+static const u_char t_parentdir_entry[] = ""
+"<tr>"
+"<td class=\"link\"><a href=\"../?C=N&amp;O=A\">Parent directory/</a></td>"
+"<td class=\"size\">-</td>"
+"<td class=\"date\">-</td>"
+"</tr>"
+"\n"
+buffer: v
+buffer: N
+buffer: N
+buffer: O
+buffer: O
+buffer: N
+buffer: N
+buffer: E
+buffer: E
+buffer: -
+;
+buffer: v
+buffer: t
+buffer: t
+buffer: 0
+buffer: 0
+buffer: 7
+buffer: 7
+buffer: _
+buffer: _
+buffer: l
+buffer: l
+buffer: i
+buffer: i
+buffer: s
+buffer: s
+buffer: t
+buffer: t
+buffer: 2
+buffer: 2
+buffer: -
+static const u_char t07_list2[] = ""
+"</tbody>"
+"</table>"
+buffer: v
+buffer: t
+buffer: t
+buffer: 0
+buffer: 0
+buffer: 8
+buffer: 8
+buffer: _
+buffer: _
+buffer: f
+buffer: f
+buffer: o
+buffer: o
+buffer: o
+buffer: o
+buffer: t
+buffer: t
+buffer: 1
+buffer: 1
+buffer: -
+;
+static const u_char t08_foot1[] = ""
+"</body>"
+"</html>"
+;
+#define NFI_TEMPLATE_SIZE (0 \
+	+ nfi_sizeof_ssz(t05_body2) \
+	+ nfi_sizeof_ssz(t06_list1) \
+	+ nfi_sizeof_ssz(t_parentdir_entry) \
+	+ nfi_sizeof_ssz(t07_list2) \
+	+ nfi_sizeof_ssz(t08_foot1) \
+	+ nfi_sizeof_ssz(t01_head1) \
+	+ nfi_sizeof_ssz(t02_head2) \
+	+ nfi_sizeof_ssz(t03_head3) \
+	+ nfi_sizeof_ssz(t04_body1) \
+	)
