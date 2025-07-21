0e1670aa99e17526fa64b841195925c377a1196b
惊蛰 <100140586+o-i-i-o@users.noreply.github.com>
2025-07-21 14:54:59 +0800

    Add files via upload

---
+++ template.h
@@ -0,0 +1,56 @@
+/* 此文件自动生成，请勿修改! */
+static const u_char t02_head2[] = ""
+"\n"
+"<title>目录索引"
+;
+static const u_char t03_head3[] = ""
+"</title>"
+"\n"
+"</head>"
+;
+static const u_char t04_body1[] = ""
+"<body>"
+"<h1>目录索引"
+;
+static const u_char t05_body2[] = ""
+"</h1>"
+"\n"
+;
+static const u_char t06_list1[] = ""
+"<table id=\"list\">"
+"<thead>"
+"<tr>"
+"<th style=\"width:55%\"><a href=\"?C=N&amp;O=A\">文件名称</a>&nbsp;<a href=\"?C=N&amp;O=D\">&nbsp;&darr;&nbsp;</a></th>"
+"<th style=\"width:20%\"><a href=\"?C=S&amp;O=A\">文件大小</a>&nbsp;<a href=\"?C=S&amp;O=D\">&nbsp;&darr;&nbsp;</a></th>"
+"<th style=\"width:25%\"><a href=\"?C=M&amp;O=A\">上传日期</a>&nbsp;<a href=\"?C=M&amp;O=D\">&nbsp;&darr;&nbsp;</a></th>"
+"</tr>"
+"</thead>"
+"\n"
+"<tbody>"
+;
+static const u_char t_parentdir_entry[] = ""
+"<tr>"
+"<td class=\"link\"><a href=\"../?C=N&amp;O=A\">上级目录/</a></td>"
+"<td class=\"size\">-</td>"
+"<td class=\"date\">-</td>"
+"</tr>"
+"\n"
+;
+static const u_char t07_list2[] = ""
+"</tbody>"
+"</table>"
+;
+static const u_char t08_foot1[] = ""
+"</body>"
+"</html>"
+;
+#define NFI_TEMPLATE_SIZE (0 \
+	+ nfi_sizeof_ssz(t02_head2) \
+	+ nfi_sizeof_ssz(t03_head3) \
+	+ nfi_sizeof_ssz(t04_body1) \
+	+ nfi_sizeof_ssz(t05_body2) \
+	+ nfi_sizeof_ssz(t06_list1) \
+	+ nfi_sizeof_ssz(t_parentdir_entry) \
+	+ nfi_sizeof_ssz(t07_list2) \
+	+ nfi_sizeof_ssz(t08_foot1) \
+	)
